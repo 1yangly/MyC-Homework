27da4b7f9aebed2467da0e388203d0011a0bdbdf
1yangly <cloudsunfolding@gmail.com>
2024-03-20 19:00:52 +0800

    第四次作业

---
+++ Assignment4/4-1/4-1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    private Node<T> head;
+
+    public void Add(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if (head == null)
+            head = newNode;
+        else
+        {
+            Node<T> current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.Add(3);
+        list.Add(7);
+        list.Add(1);
+        list.Add(10);
+
+        Console.WriteLine("Printing list elements:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        int sum = 0;
+
+        list.ForEach(x =>
+        {
+            max = Math.Max(max, x);
+            min = Math.Min(min, x);
+            sum += x;
+        });
+
+        Console.WriteLine($"Max: {max}");
+        Console.WriteLine($"Min: {min}");
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
+
---
+++ Assignment4/4-2/4-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+public class AlarmClock
+{
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    public void Start(int durationSeconds)
+    {
+        Console.WriteLine($"闹钟已设置，将在 {durationSeconds} 秒后响铃。");
+        Thread.Sleep(durationSeconds * 1000);
+
+        OnTick();
+        OnAlarm();
+    }
+
+    protected virtual void OnTick()
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+    }
+
+    protected virtual void OnAlarm()
+    {
+        Alarm?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AlarmClock alarmClock = new AlarmClock();
+        alarmClock.Tick += (sender, e) => Console.WriteLine("嘀嗒，嘀嗒");
+        alarmClock.Alarm += (sender, e) => Console.WriteLine("该起床了！");
+
+        // 设置闹钟
+        alarmClock.Start(5);
+    }
+}
